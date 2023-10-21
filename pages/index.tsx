81e6b6aa6ffd9b4568ccc8370df9705fc7a7ffd5
vivianequeiroz <vivianedsqueiroz@gmail.com>
2023-10-21 13:35:05 -0300

    feat: add carousel with keen-slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,39 @@
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
 
+import { useKeenSlider } from "keen-slider/react";
+
+import "keen-slider/keen-slider.min.css";
+
 import shirt1 from "../assets/shirts/1.svg";
 import shirt2 from "../assets/shirts/2.svg";
 import shirt3 from "../assets/shirts/3.svg";
 import shirt4 from "../assets/shirts/4.svg";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={shirt1} alt="Shirt 1" width={520} height={480} />
         <footer>
           <strong>Shirt 1</strong>
           <span>$ 69,99</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt2} alt="Shirt 2" width={520} height={480} />
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt3} alt="Shirt 3" width={520} height={480} />
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={shirt4} alt="Shirt 4" width={520} height={480} />
       </Product>
     </HomeContainer>
--- styles/pages/home.ts
+++ styles/pages/home.ts
@@ -2,7 +2,6 @@ import { styled } from "..";
 
 export const HomeContainer = styled("main", {
   display: "flex",
-  gap: "3rem",
   maxWidth: "calc(100vw - ((100vw - 1100px)/2))",
   marginLeft: "auto",
   minHeight: 656,
@@ -16,7 +15,6 @@ export const Product = styled("a", {
   background: "linear-gradient(100deg, #8A2BE2 0%, #FF69B4 100%)",
   borderRadius: 8,
   cursor: "pointer",
-  padding: "0.25rem",
   position: "relative",
 
   img: {
